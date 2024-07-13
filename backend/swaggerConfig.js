5771589f9c8aa51be2513a19b8c4d759f6dbd764
mdekabs <1mawoda@gmail.com>
2024-07-13 20:42:00 +0100

    bind to 0.0.0.0

--- backend/swaggerConfig.js
+++ backend/swaggerConfig.js
@@ -8,7 +8,7 @@ export const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api/v1",
+        url: "http://0.0.0.0:4000/api/v1",
       },
     ],
     components: {
