e2c60a60f3465e8d6d8353e4fc12a2e37cb3d9e1
mdekabs <1mawoda@gmail.com>
2024-07-22 21:20:32 +0100

    adjust a comment

--- backend/app.js
+++ backend/app.js
@@ -63,7 +63,6 @@ async function connectToDatabase() {
 
 connectToDatabase();
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
