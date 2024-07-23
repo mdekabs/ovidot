289ca7587dd9845efe60f763481aa5377065e27e
mdekabs <1mawoda@gmail.com>
2024-07-23 07:41:10 +0100

    including dark mode to the swaggerUi

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ import helmet from "helmet";
 import fs from "fs";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import swaggerUiThemes from "swagger-ui-themes";
 import { Pagination, checkCache, cacheResponse } from "./middlewares/index.js";
 import { swaggerOptions } from "./swaggerConfig.js";
 import { authRoute, userRoute, cycleRoute, pregnancyRoute, moodRoute, emergencyContactRoute } from "./routes/index.js";
@@ -40,7 +41,9 @@ app.use(morgan('combined', { stream: logStream }));
 
 // Swagger setup
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+  customCss: swaggerUiThemes.getTheme('dark-mode')
+}));
 
 // Route definitions
 app.use("/api/v1/auth", authRoute);
