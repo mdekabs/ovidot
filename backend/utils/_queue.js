64fa8a46b112ec038a94521401e1c324d32baa8d
mdekabs <1mawoda@gmail.com>
2024-07-27 22:43:31 +0100

    resolving email queue bug

--- backend/utils/_queue.js
+++ backend/utils/_queue.js
@@ -1,19 +1,10 @@
 import "dotenv/config";
 import Queue from 'bull';
 
-
-//dotenv.config();
-
-
-const RH = process.env.REDIS_HOST;
-const RP = process.env.REDIS_PORT;
-console.log(RH);
-
 const emailQueue = new Queue('emailQueue', {
-    redis: {
-        host: process.env.RH,
-        port: process.env.RP
-    }
+    redis: process.env.REDIS_URL
 });
+
+console.log(`Redis URL: ${process.env.REDIS_URL}`);
 
 export { emailQueue };
