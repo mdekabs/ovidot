c016510eb749c284e57b15724d2f70a743df1fb5
mdekabs <1mawoda@gmail.com>
2024-08-07 14:26:24 +0100

    need to decrypt before checking for cycle data

--- backend/utils/_month.js
+++ backend/utils/_month.js
@@ -1,13 +1,22 @@
 import { Cycle } from '../models/index.js';
+import { decryptData } from '../utils/index.js'; // Ensure this path is correct
+import dotenv from "dotenv";
+
+dotenv.config();
+const encryptionKey = process.env.ENCRYPTION_KEY;
 
 export const checkCycleExistsForMonth = async (userId, startDate) => {
     const startDateObj = new Date(startDate);
     const month = `${startDateObj.getFullYear()}-${startDateObj.getMonth() + 1}`;
 
-    const existingCycle = await Cycle.findOne({
-        userId: userId,
-        month: month
-    });
+    const cycles = await Cycle.find({ userId });
 
-    return existingCycle !== null;
+    for (const cycle of cycles) {
+        const decryptedMonth = decryptData(cycle.month, encryptionKey);
+        if (decryptedMonth === month) {
+            return true;
+        }
+    }
+
+    return false;
 };
