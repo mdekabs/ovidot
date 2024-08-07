0003ea82f3cf4a5b6f001d0160fa8b9b96a91c31
mdekabs <1mawoda@gmail.com>
2024-08-07 10:50:10 +0100

    resolving encryption issue with the cycle model

--- backend/utils/_security.js
+++ backend/utils/_security.js
@@ -1,16 +1,26 @@
-// security.js
 import crypto from 'crypto';
 
 export const encryptData = (data, encryptionKey) => {
-    const cipher = crypto.createCipher('aes-256-cbc', encryptionKey);
-    let encrypted = cipher.update(data, 'utf8', 'hex');
+    if (!data) return data;
+
+    const iv = crypto.randomBytes(16); // Generate a random initialization vector
+    const key = crypto.createHash('sha256').update(encryptionKey).digest(); // Hash the encryption key
+
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let encrypted = cipher.update(data.toString(), 'utf8', 'hex'); // Convert data to string to handle non-string data types
     encrypted += cipher.final('hex');
-    return encrypted;
+    return `${iv.toString('hex')}:${encrypted}`; // Return iv and encrypted data
 };
 
 export const decryptData = (encryptedData, encryptionKey) => {
-    const decipher = crypto.createDecipher('aes-256-cbc', encryptionKey);
-    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    if (!encryptedData) return encryptedData; // If data is undefined or null, return it as is
+
+    const [ivHex, encrypted] = encryptedData.split(':'); // Separate iv from the encrypted data
+    const iv = Buffer.from(ivHex, 'hex');
+    const key = crypto.createHash('sha256').update(encryptionKey).digest(); // Hash the encryption key
+
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 };
