b2bb61bc47277a381532379b03f8113dcbeec51f
mdekabs <1mawoda@gmail.com>
2024-08-14 12:10:48 +0100

    configured the dotenv in the worker file

--- backend/worker.js
+++ backend/worker.js
@@ -1,6 +1,10 @@
 import { emailQueue, _emailProcessor } from './utils/index.js';
 import dotenv from "dotenv";
 
+
+
+dotenv.config();
+
 emailQueue.process(_emailProcessor);
 
 console.log('Email worker is running and processing jobs...');
